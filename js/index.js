a4e9a8d4cdab46aa0d07ae45d106473e1b78e6f9
PEI-YUN LEE <may1092200258@gmail.com>
2019-12-18 10:16:26 +0800

    m

--- js/index.js
+++ js/index.js
@@ -7,4 +7,15 @@ $(document).ready(function () {
     cssEase: 'linear',
     arrows: false,
   });
+
+  let i=0;
+  $('#hamberger').click(function (){
+    i++;
+    if(i==1)
+      $('.menu').toggleClass('menu_active');
+    else{
+      $('.menu').removeClass('menu_active');
+      i=0
+    }
+  })
 })
